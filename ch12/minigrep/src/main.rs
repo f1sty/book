0f9c8dd8507c73e25091c43d43236f392f860543
Yurii Skrynnykov <truef1s7@gmail.com>
2023-07-21 16:11:38 +0300

    Update minigrep

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,10 +1,21 @@
 use std::env;
+use std::fs;
 use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let regex = &args[1];
-    let path = Path::new(&args[2]);
+    let (regexp, file_path) = parse_config(&args);
 
-    println!("searching for '{regex}' at file {path:?}");
+    println!("searching for '{regexp}' at file {file_path:?}\n");
+
+    let contents = fs::read_to_string(file_path).expect("Couldn't open file {path:?}");
+
+    print!("file text:\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &Path) {
+    let regex = &args[1];
+    let file_path = Path::new(&args[2]);
+
+    (regex, file_path)
 }
